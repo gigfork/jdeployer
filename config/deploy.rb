b030367ffe43dd784b027ef878e87f4ac17fdad0
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2012-06-22 12:46:52 +0530

    Trying to use the tomcat manager for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :user, "rockyj"
 set :application, "jdeployer"
 
 set :java_home, "/home/#{user}/jdk1.7.0_04"
-set :tomcat_home, "/home/#{user}/Apps/apache-tomcat-7.0.23"
+set :tomcat_home, "/home/#{user}/Apps/apache-tomcat-7.0.28"
 set :maven_home, "/home/#{user}/Apps/apache-maven-3.0.3"
 set :deploy_to, "/home/#{user}/Temp/#{application}"
 
@@ -22,13 +22,13 @@ namespace :tomcat do
   task :build_deploy_restart do
     puts "==================Building with Maven======================"
     run "export JAVA_HOME=#{java_home} && cd #{deploy_to}/current && #{maven_home}/bin/mvn clean package"
-    puts "==================Stop Tomcat======================"
-    run "export JAVA_HOME=#{java_home} && #{tomcat_home}/bin/shutdown.sh"
+    #puts "==================Stop Tomcat======================"
+    #run "export JAVA_HOME=#{java_home} && #{tomcat_home}/bin/shutdown.sh"
     puts "==================Copy war to Tomcat======================"
-    run "cp #{deploy_to}/current/target/#{application}*.war #{tomcat_home}/webapps/#{application}.war"
-    puts "==================Start Tomcat======================"
-    run "export JAVA_HOME=#{java_home} && #{tomcat_home}/bin/startup.sh"
-    puts "==========================================================="
+    run "curl --upload-file #{deploy_to}/current/target/#{application}*.war --user tomcatt:tomcatt http://$CAPISTRANO:HOST$:8080/manager/text/deploy?path=/#{application}"
+    #puts "==================Start Tomcat======================"
+    #run "export JAVA_HOME=#{java_home} && #{tomcat_home}/bin/startup.sh"
+    #puts "==========================================================="
   end
 end
 
