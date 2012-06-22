c24267dc2cd80b83761fd45cafbedbfc5a08ff71
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2012-06-22 13:24:14 +0530

    Trying to add environments / stages

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,3 @@
-server "localhost", :web, :app, :db, primary: true
-
-set :user, "rockyj"
-set :application, "jdeployer"
-
-set :java_home, "/home/#{user}/jdk1.7.0_04"
-set :tomcat_home, "/home/#{user}/Apps/apache-tomcat-7.0.28"
-set :tomcat_manager, "tomcatt"
-set :tomcat_manager_password, "tomcatt"
-set :maven_home, "/home/#{user}/Apps/apache-maven-3.0.3"
-set :deploy_to, "/home/#{user}/Temp/#{application}"
-
-#set :deploy_via, :remote_cache
-set :use_sudo, false
-
 set :scm, "git"
 set :repository, "git@github.com:rocky-jaiswal/#{application}.git"
 set :branch, "master"
@@ -20,16 +5,28 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-namespace :tomcat do
-  task :build_deploy_restart do
-    puts "==================Building with Maven======================"
-    run "export JAVA_HOME=#{java_home} && cd #{deploy_to}/current && #{maven_home}/bin/mvn clean package"
-    puts "==================Undeploy war======================"
-    run "curl --user #{tomcat_manager}:#{tomcat_manager_password} http://$CAPISTRANO:HOST$:8080/manager/text/deploy?path=/#{application}"
-    puts "==================Deploy war to Tomcat======================"
-    run "curl --upload-file #{deploy_to}/current/target/#{application}*.war --user #{tomcat_manager}:#{tomcat_manager_password} http://$CAPISTRANO:HOST$:8080/manager/text/deploy?path=/#{application}"
-  end
-end
-
-after "deploy", "tomcat:build_deploy_restart" # keep only the last 5 releases
-after "tomcat:build_deploy_restart", "deploy:cleanup" # keep only the last 5 releases
+define_stage :local_development do
+    roles.clear
+    server "localhost", :app
+    set :user, "rockyj"
+    set :application, "jdeployer"
+    set :java_home, "/home/#{user}/jdk1.7.0_04"
+    set :tomcat_home, "/home/#{user}/Apps/apache-tomcat-7.0.28"
+    set :tomcat_manager, "tomcatt"
+    set :tomcat_manager_password, "tomcatt"
+    set :maven_home, "/home/#{user}/Apps/apache-maven-3.0.3"
+    set :deploy_to, "/home/#{user}/Temp/#{application}"
+    set :use_sudo, false
+    namespace :tomcat do
+      task :build_deploy_restart do
+        puts "==================Building with Maven======================"
+        run "export JAVA_HOME=#{java_home} && cd #{deploy_to}/current && #{maven_home}/bin/mvn clean package"
+        puts "==================Undeploy war======================"
+        run "curl --user #{tomcat_manager}:#{tomcat_manager_password} http://$CAPISTRANO:HOST$:8080/manager/text/deploy?path=/#{application}"
+        puts "==================Deploy war to Tomcat======================"
+        run "curl --upload-file #{deploy_to}/current/target/#{application}*.war --user #{tomcat_manager}:#{tomcat_manager_password} http://$CAPISTRANO:HOST$:8080/manager/text/deploy?path=/#{application}"
+      end
+    end
+    after "update", "tomcat:build_deploy_restart" # keep only the last 5 releases
+    after "tomcat:build_deploy_restart", "deploy:cleanup" # keep only the last 5 releases
+do
